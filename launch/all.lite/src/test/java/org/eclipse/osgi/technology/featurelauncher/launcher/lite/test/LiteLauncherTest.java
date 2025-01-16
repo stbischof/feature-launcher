8b86877b66a393fed029c36855b85244be1f8aec
Stefan Bischof <stbischof@bipolis.org>
2025-01-16 19:44:41 +0100

    add license header in java files

--- launch/all.lite/src/test/java/org/eclipse/osgi/technology/featurelauncher/launcher/lite/test/LiteLauncherTest.java
+++ launch/all.lite/src/test/java/org/eclipse/osgi/technology/featurelauncher/launcher/lite/test/LiteLauncherTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2024 Kentyou and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Kentyou - initial implementation
+ */
 package org.eclipse.osgi.technology.featurelauncher.launcher.lite.test;
 
 import org.eclipse.osgi.technology.featurelauncher.launcher.tests.SimpleCliLaunchTest;
